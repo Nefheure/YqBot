c1095f4827d88d750a3dc209fbba64f6c5daebc3
Nefheure <43937013+Nefheure@users.noreply.github.com>
2018-11-22 21:09:43 +0100

    Update yqbot.js

--- .gitignore/yqbot.js
+++ .gitignore/yqbot.js
@@ -4,13 +4,39 @@ const prefix = "-";
 
 var bot = new Discord.Client();
 
+let statues = ['-aide', 'Par nefer',];
 
-bot.on("ready", function()  {
-    console.log("Yqbot Opperationnel !");
-    bot.user.setActivity("-aide")
-    bot.user.setStatus("online")
+bot.on('ready', () => {
+    setInterval(function() {
+     let status = statues[Math.floor(Math.random()*statues.length)];
+       bot.user.setPresence({ game: { name: status }, status: 'online'});
+  }, 1000)
+ console.log("Yqbot Opperationnel !"); 
+})
 
-});
+
+
+bot.on('message', message => {
+    if(message.content === prefix + "stats"){
+
+    var userCreateDate = message.author.createdAt.toString().split(" ");
+    var msgauthor = message.author.id;
+    
+     var stats_embed = new Discord.RichEmbed()
+      .setAuthor("Statistique Par **Yqbot**", bot.user.avatarURL)
+      .setColor("#000000")
+      .setDescription("Voici t'es informations sur ton compte")
+      .setTitle(`Statistiques pour ${message.author.username}`)
+      .addField("tu a cree ton compte au dates suivante", userCreateDate[1] + ' ' + userCreateDate[2] + " " + userCreateDate[3])
+      .addField(`ton identifiant et le suivant`, msgauthor, true)
+      .setFooter("Versia, created by Nefer")
+      .setThumbnail(message.author.avatarURL)
+      .setTimestamp()
+      message.reply("tu peut regarder t'es message priver, tu viens de recevoir t'es informations personnel !")
+      message.author.send({embed: stats_embed}); 
+}});
+
+
 
 bot.on('message', message => {
 
@@ -19,10 +45,12 @@ bot.on('message', message => {
           .setColor("#000000")
           .setTitle("Commande **aide**")
           .setDescription("Vous permet de Vous aider dans mes commande.")
-          .addField("-aide", "Vous Montre le Stockage de command.")
-          .addField("-youtube", "Vous envoie un lien vers la chaine youtube de MrYqoshhFX")
-          .addField("-instagram", "Vous envoie un lien ver l'instagram de MrYqoshhFX.")
+          .addField("``-aide``", "Vous Montre le Stockage de command.")
+          .addField("``-youtube``", "Vous envoie un lien vers la chaine youtube de MrYqoshhFX")
+          .addField("``-instagram``", "Vous envoie un lien ver l'instagram de MrYqoshhFX.")
+          .addField("``-stats``", "vous envoie vos informations personnel en priver")
           .setFooter("YqBot, par Neferr.")
+          .setTimestamp()
           message.channel.sendMessage(aide_embed);
     }
 });
@@ -32,9 +60,10 @@ bot.on('message', message => {
     if(message.content === prefix + "youtube"){
         var ytb_embed = new Discord.RichEmbed()
         .setColor("#000000")
-        .setTitle("Voici la Chaine Youtube de **YqoshhFX** dans le lien ci-dessous")
-        .addField("*lien :*", "https://www.youtube.com/channel/UCEswwt_xabebAcvuiwGdgrA")
+        .setDescription("Voici la Chaine Youtube de **YqoshhFX** dans le lien ci-dessous")
+        .addField("https://www.youtube.com/channel/UCEswwt_xabebAcvuiwGdgrA")
         .setFooter("YqBot, par Neferr.")
+        .setTimestamp()
         message.channel.sendMessage(ytb_embed);
     }
 });
@@ -44,9 +73,10 @@ bot.on('message', message => {
     if(message.content === prefix + "instagram"){
         var insta_embed = new Discord.RichEmbed()
         .setColor("#000000")
-        .setTitle("Voici l'Instagram de **YqoshhFX** tu peut le REjoindre avec le lien c-dessous")
-        .addField("*lien*", "https://instagram.com/yqoshhfx?utm_source=ig_profile_share&igshid=24fyxnfuqm2u")
+        .setDescription("Voici l'Instagram de **YqoshhFX** tu peut le REjoindre avec le lien c-dessous")
+        .addField("https://instagram.com/yqoshhfx?utm_source=ig_profile_share&igshid=24fyxnfuqm2u")
         .setFooter("YqBot, par Neferr.")
+        .setTimestamp()
         message.channel.sendMessage(insta_embed);
     }
 });
